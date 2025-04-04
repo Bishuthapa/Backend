ea9a42eb3a0decfd70ad81c2bcfee4b8c96a433d
Bishuthapa <thapabishu435@gmail.com>
2025-04-04 23:52:03 +0545

    //

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,59 @@
 import mongoose ,{Schema} from "mongoose";
 
 const userSchema = new Schema({
+    username: {
+        name: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+    },
     
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+
+    avatar: {
+        type: String, //URL
+        required: true
+
+    },
+
+    coverImage: {
+        type: String, //URL
+
+    },
+
+    watchHistory : [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+
+
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+
+    refreshToken : {
+        type: String
+    }
+
+},
+{
+    timestamps: true
 })
 
 
