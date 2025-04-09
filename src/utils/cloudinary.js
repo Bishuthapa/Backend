71c58ae57045443bbe0e46029b911554dc020488
Bishuthapa <thapabishu435@gmail.com>
2025-04-09 23:45:15 +0545

    configure the multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: CLOUDINARY_API_SECRET  // Click 'View API Keys' above to copy your API secret
+    });
+    
+    // Upload an image
+    const uploadOnCloudinary = async (localPath) =>{
+    try {
+        if (!localPath) return null;
+        const response = await cloudinary.uploader.upload(localPath, {
+            resource_type: "auto"
+        });
+
+        return response;
+    } catch (error) {
+
+        fs.unlinkSync(localPath);
+        return null;
+        console.log(error);
+    }
+    }
+
+    export default uploadOnCloudinary;
