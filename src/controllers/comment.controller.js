7a9d5dde7d3242ea0a7185dd6d626fb26aaa10b3
Bishu Thapa <thapabishu435@gmail.com>
2025-08-24 23:22:36 +0545

    add getVideoComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,64 +1,78 @@
 import asyncHandler from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiRespones";
-import {Comment} from "../models/comment.model";
+import { Comment } from "../models/comment.model";
 import mongoose from "mongoose";
-import {Video} from "../models/video.model";
-const getVideoComment = asyncHandler(async (req , res) => {
+import { Video } from "../models/video.model";
+import { User } from "../models/user.model";
+const getVideoComment = asyncHandler(async (req, res) => {
+  //get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
 
-    //get all comments for a video
-    const {videoId} = req.params;
-    const {page = 1, limit = 10} = req.query;
+  if (!videoId) {
+    throw new ApiError(400, "Video id is required");
+  }
+  //here the aggreation pipeline started
 
-    if(!videoId){
-        throw new ApiError(400, "Video id is required");
-    }
-    //here the aggreation pipeline started
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "User",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+        $unwind : "owner",
+    },
+    {
+        $sort: {createdAt : -1},
+    },
+    {
+        $project : {
+            content : 1,
+            owner: 1,
+            createdAt : 1,
+        },
+    },
+  ]);
 
-    const comments = await Comment.aggregate([
-        {
-            $match: {
-                video : 
 
-            }
-        },{},{}
-    ])
+  if(!comments?.length){
+    throw new ApiError(404, "No comments found for this video");
+  }
+  
+  res
+  .status(200)
+  .json(
+      new ApiResponse(
+          200,
+          comments,
+          "Fetch video comments successfully"
 
-    
-
-    // const comments = await Comment.find({video: videoId}).
-    // populate("Owner", "avatar").
-    // sort({createdAt: -1});
-
-    // res
-    // .status(200)
-    // .json(
-    //     new ApiResponse(
-    //         200,
-    //         comments,
-    //         "Fetch video comments successfully"
-
-    //     )
-    // )
-
-
+      )
+  )
 
 
 });
 
-const addComment = asyncHandler(async (req, res) => {
+const addComment = asyncHandler(async (req, res) => {});
+const updateComment = asyncHandler(async (req, res) => {});
+const deleteComment = asyncHandler(async (req, res) => {});
 
-}) ;
-const updateComment = asyncHandler(async (req, res) =>{
-
-}) ;
-const deleteComment = asyncHandler(async (req, res) => {
-
-});
-
-export {
-    getVideoComment,
-    addComment,
-    updateComment,
-    deleteComment
-}
+export { getVideoComment, addComment, updateComment, deleteComment };
