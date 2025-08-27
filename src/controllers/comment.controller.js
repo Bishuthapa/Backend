aafe5c47b8b3b14ca5d31e829ded8057687668b0
Bishu Thapa <thapabishu435@gmail.com>
2025-08-27 17:08:47 +0545

    add addcomment controller function

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,6 +5,8 @@ import { Comment } from "../models/comment.model";
 import mongoose from "mongoose";
 import { Video } from "../models/video.model";
 import { User } from "../models/user.model";
+
+
 const getVideoComment = asyncHandler(async (req, res) => {
   //get all comments for a video
   const { videoId } = req.params;
@@ -15,7 +17,7 @@ const getVideoComment = asyncHandler(async (req, res) => {
   }
   //here the aggreation pipeline started
 
-  const comments = await Comment.aggregate([
+  const comments = await Comment.aggregate([ //here it returns a object array so comments is a array
     {
       $match: {
         video: new mongoose.Types.ObjectId(videoId),
@@ -23,7 +25,7 @@ const getVideoComment = asyncHandler(async (req, res) => {
     },
     {
       $lookup: {
-        from: "User",
+        from: "users",
         localField: "owner",
         foreignField: "_id",
         as: "owner",
@@ -38,18 +40,20 @@ const getVideoComment = asyncHandler(async (req, res) => {
       },
     },
     {
-        $unwind : "owner",
+        $unwind : "$owner",
     },
     {
         $sort: {createdAt : -1},
     },
     {
         $project : {
-            content : 1,
-            owner: 1,
-            createdAt : 1,
+            content : 1, // it is a content of a comment
+            owner: 1, // here it have username and avatar
+            createdAt : 1, //it show the latest comment 
         },
     },
+    { $skip: (page - 1) * limit },   // pagination: skip previous pages
+    { $limit: parseInt(limit) }, //set the limit for the per page
   ]);
 
 
@@ -71,7 +75,46 @@ const getVideoComment = asyncHandler(async (req, res) => {
 
 });
 
-const addComment = asyncHandler(async (req, res) => {});
+const addComment = asyncHandler(async (req, res) => {
+
+  //get the video id
+
+  const {videoId} = req.params;
+
+  if(!videoId){
+    throw new ApiError(401, "Video ID is required");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if(!video){
+    throw new ApiError(401, "Requested video does not exists");
+  }
+  
+  const {content}  = req.body;
+
+  if(!content || content?.trim() === ""){
+    throw new ApiError(401, "Content is required");
+  }
+
+ const comment = await Comment.create({
+  content,
+  video: videoId,
+  owner : req.user?._id,
+ })
+
+const populateComment = await comment.populate("owner", "email");
+  res
+  .status(201)
+  .json(
+    new ApiResponse(
+      200, populateComment,  "sucessfully commented on the video"
+    )
+  )
+
+
+  
+});
 const updateComment = asyncHandler(async (req, res) => {});
 const deleteComment = asyncHandler(async (req, res) => {});
 
