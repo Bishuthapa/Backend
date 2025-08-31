dd07a6225d373f3c6b23f29587d1f57702f6cf89
Bishu Thapa <thapabishu435@gmail.com>
2025-08-31 11:47:44 +0545

    add the like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,3 +4,45 @@ import {ApiError} from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiRespones";
 import { Like } from "../models/like.model.js";
 
+const toggleVideoLike = asyncHandler( async (req , res) => {
+    const  {videoId} = req.params;
+    if(!videoId){
+        throw new ApiError(401, "Video ID is required");
+    }
+
+    const existingLike = await Like.findOne({
+        video : videoId,
+        likedBy : req.user._id
+    })
+
+    let isLiked;
+    let message;
+    if(existingLike){
+
+       await Like.findByIdAndDelete(existingLike._id);       
+       isLiked = false;
+       message = "Video unliked successfully";
+
+    }else{
+         await Like.create({
+            video : videoId,
+            likedBy : req.user._id
+        })
+        isLiked = true;
+        message = "Video liked successfully";
+    }
+
+     const totalLikes = await Like.countDocuments({ video: videoId });
+
+     return res
+        .status(200)
+        .json(new ApiResponse(200, {isLiked, totalLikes}, message));
+    });
+
+const toggleCommentLike = asyncHandler( async (req , res) => {});
+
+const toggleTweetLike = asyncHandler( async (req , res) => {});
+
+const getLikedVideos = asyncHandler( async (req , res) => {});
+
+export {toggleVideoLike}
