378ea9725488c1d67cd21b206e0aea6db8e3e5b0
Bishu Thapa <thapabishu435@gmail.com>
2025-09-02 09:47:49 +0545

    add tweets routes

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middelware.js";
+import {
+    createTweet,
+    getUserTweets, 
+    updateTweet, 
+    deleteTweet} from "../controllers/tweet.controller.js"
+
+const router = Router();
+router.use(verifyJWT);
+router
+    .route("/:userId/tweets")
+    .post(createTweet)
+    .get(getUserTweets);
+
+router
+    .route("/:userId/tweets/:tweetId")
+    .patch(updateTweet)
+    .delete(deleteTweet);
+
+export default router;
