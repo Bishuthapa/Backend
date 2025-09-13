30d85722a1b3b5b13c439dbf3fce028b8791daa8
Bishu Thapa <thapabishu435@gmail.com>
2025-09-13 16:55:34 +0545

    update the middleware

--- src/DB/index.js
+++ src/DB/index.js
@@ -10,8 +10,6 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-         .then(() => console.log("Connected!"))
-  .catch(err => console.error("DB connection error:", err));
         console.log(`\n MongoDB connected: ${connectionInstance.connection.host}`);
 
     }
